308935f1031f9c95a2526f2c6b7f99372d30e03e
Quynh Ngo <QuynhTNgo@live.com>
2022-11-30 19:34:02 -0700

    Finish displayFacilities method

--- Facilities.py
+++ Facilities.py
@@ -1,3 +1,22 @@
 class Facility:
-    def __init__(self, facilityName):
-        self.__facilityName = facilityName
+    def __init__(self, name):
+        self.__name = name
+    
+    def getName(self):
+        return self.__name
+
+    def setName(self, newName):
+        self.__name = newName
+
+    def displayFacilities(self):
+        f = open("facilities.txt", "r")
+        for line in f.readlines():
+            print(line)
+        f.close()
+
+    def addFacilities(self):
+        pass
+
+    def writeListOfFacilitiesToFile(self):
+        pass
+
