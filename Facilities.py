9eae5a55bf23e74cc1fc1d5468d537a49e245986
Quynh Ngo <QuynhTNgo@live.com>
2022-11-30 20:12:02 -0700

    addFicilities and writeListOfFacilities done

--- Facilities.py
+++ Facilities.py
@@ -1,4 +1,5 @@
 class Facility:
+    facilities_list = []
     def __init__(self, name):
         self.__name = name
     
@@ -12,11 +13,17 @@ class Facility:
         f = open("facilities.txt", "r")
         for line in f.readlines():
             print(line)
+            self.facilities_list.append(line)
         f.close()
 
     def addFacilities(self):
-        pass
+        newFacilityName = input("Enter facility name: ")
+        self.facilities_list.append("\n" + newFacilityName)
+        return newFacilityName
+
 
     def writeListOfFacilitiesToFile(self):
-        pass
-
+        f = open("facilitiestest.txt", "w")
+        for item in self.facilities_list:
+            f.writelines(item)
+        f.close()
